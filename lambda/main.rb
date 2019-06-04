c3251e2dfd8c4d501f3e6257ba953a7aca16fcb4
kSunaga <jokers4140@gmail.com>
2019-06-04 16:45:22 +0900

    apiと繋ぎこみ

--- lambda/main.rb
+++ lambda/main.rb
@@ -21,28 +21,21 @@ def self.convert_price_to_int(element)
   element.delete('¥').delete(',').to_i
 end
 
-# 配列
-# products = Scraping.getKeywords
-
 agent = Mechanize.new {|agent|
   agent.user_agent_alias = USER_AGENT
 }
 
 page = agent.get(SEARCH_URL)
 form = page.form
-products = {name: "ipad pro", min_place: 20000, max_place: 40000}
+products = JSON.parse(Scraping.getKeywords.body)
+products["items"].each do |p|
+  form['keyword'] = p["keyword"]
 
-# if products.nil?
-  form['keyword'] = products[:name]
-# else
-#   products.each do |k|
-#     form['keyword'] = k[:name]
-#   end
-# end
-result = form.click_button
+  result = form.click_button
 
-result.search('.items-box-body').each do |r|
-  if products[:min_place] < get_product_price(r) && get_product_price(r) < products[:max_place]
-    puts ("#{get_product_name(r)}の価格は#{get_product_price(r)}です。")
+  result.search('.items-box-body').each do |r|
+    if p["min_place"] < get_product_price(r) && get_product_price(r) < p["max_place"]
+      puts ("#{get_product_name(r)}の価格は#{get_product_price(r)}です。")
+    end
   end
 end
