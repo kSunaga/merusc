1866d435649e1777a5757cae9c3f115514d6a0c1
kSunaga <jokers4140@gmail.com>
2019-06-05 22:14:33 +0900

    配列を文字列にして返す

--- lambda/slack.rb
+++ lambda/slack.rb
@@ -3,8 +3,8 @@ require 'slack-notifier'
 module Slack
   WEBHOOK_URL = 'https://hooks.slack.com/services/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'.freeze
 
-  def self.post_message
+  def self.post_message(message)
     notifier = Slack::Notifier.new WEBHOOK_URL
-    notifier.ping "test message"
+    notifier.ping message.join("\n")
   end
 end
